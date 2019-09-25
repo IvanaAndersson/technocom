a3261756ac5232ddf05a538b2bff3b2ce7d8e01d
Ivana Andersson <ivana.p.kostova@gmail.com>
2019-09-25 12:18:34 +0300

    change main page nav when scrolled

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -4,3 +4,22 @@ if (dropdownMenuButton !== null) {
     dropdownMenuButton.classList.toggle("expanded");
   });
 }
+
+const logoContainer = document.querySelector("logo-container");
+const myNav = document.getElementById("navigation-desktop");
+const headLogo = document.getElementById("head-logo-picture");
+const searchButton = document.getElementById("dropdownMenuButton");
+let currentY = window.scrollY;
+let scrollDirection = "";
+
+window.addEventListener("scroll", event => {
+  // console.log(window.scrollY);
+  scrollDirection = window.scrollY > currentY ? "down" : "up";
+  currentY = window.scrollY;
+
+  if (currentY > 5) {
+    myNav.classList.add("navbar-when-scrolled");
+  } else {
+    myNav.classList.remove("navbar-when-scrolled");
+  }
+});
