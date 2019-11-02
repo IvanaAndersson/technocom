fd39275725e3ffcf5edb17cc4ea244705ad3fae5
Ivana Andersson <ivana.p.kostova@gmail.com>
2019-11-02 15:34:55 +0200

    add changes to mobile main page

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,27 +1,57 @@
 const nextButton = document.querySelector(".carousel-button--right");
 const prevButton = document.querySelector(".carousel-button--left");
 
-const productsCarousel = document.querySelector(".carousel-products");
-const eventsCarousel = document.querySelector(".carousel-events");
 const productsButton = document.querySelector(".tabs-item--products");
 const eventsButton = document.querySelector(".tabs-item--events");
+const productsCarousel = document.querySelector(".carousel-products");
+const eventsCarousel = document.querySelector(".carousel-events");
+
+const productsButtonMobile = document.querySelector(".tabs-mob-item--products");
+const eventsButtonMobile = document.querySelector(".tabs-mob-item--events");
+const productsCarouselMobile = document.getElementById(
+  "carouselProductsMobile"
+);
+const eventsCarouselMobile = document.getElementById("carouselEventsMobile");
+
+const addCarouselSwitchHandler = (
+  tabToSwitchTo,
+  tabToSwitchFrom,
+  carouselToSwitchTo,
+  carouselToSwitchFrom
+) => {
+  tabToSwitchTo.addEventListener("click", _e => {
+    tabToSwitchTo.classList.add("active-tab");
+    tabToSwitchFrom.classList.remove("active-tab");
+    carouselToSwitchTo.style.display = "block";
+    carouselToSwitchTo.classList.add("active");
+    carouselToSwitchFrom.style.display = "none";
+    carouselToSwitchFrom.classList.remove("active");
+  });
+};
 
 // when the eventsButton is clicked - reveal the .carousel-events and hide .carousel-products
-eventsButton.addEventListener("click", e => {
-  eventsButton.classList.add("active-tab");
-  productsButton.classList.remove("active-tab");
-  eventsCarousel.style.display = "block";
-  eventsCarousel.classList.add("active");
-  productsCarousel.style.display = "none";
-  productsCarousel.classList.remove("active");
-});
-
+addCarouselSwitchHandler(
+  eventsButton,
+  productsButton,
+  eventsCarousel,
+  productsCarousel
+);
+addCarouselSwitchHandler(
+  eventsButtonMobile,
+  productsButtonMobile,
+  eventsCarouselMobile,
+  productsCarouselMobile
+);
 // when the productsButton is clicked - reveal the .carousel-products and hide .carousel-events
-productsButton.addEventListener("click", e => {
-  eventsButton.classList.remove("active-tab");
-  productsButton.classList.add("active-tab");
-  eventsCarousel.style.display = "none";
-  eventsCarousel.classList.remove("active");
-  productsCarousel.style.display = "block";
-  productsCarousel.classList.add("active");
-});
+addCarouselSwitchHandler(
+  productsButton,
+  eventsButton,
+  productsCarousel,
+  eventsCarousel
+);
+addCarouselSwitchHandler(
+  productsButtonMobile,
+  eventsButtonMobile,
+  productsCarouselMobile,
+  eventsCarouselMobile
+);
